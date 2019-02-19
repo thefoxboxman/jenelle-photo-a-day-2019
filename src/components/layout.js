3173717e8e33e9daaf60405d91f3dd4e284603a6
F John De Costa <john@mayerfleur.com>
2019-02-20 10:13:42 +1100

    Added display grid to li list

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,16 +18,10 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-						textAlign: `center`,
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
+				{/* disable header
+				<Header siteTitle={data.site.siteMetadata.title} /> 
+				*/}
+        <div> 
           <main>{children}</main>
           <footer>
             © {new Date().getFullYear()}, Built by John De Costa,  "Head Web Wrangler" at  
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "gatsby-image";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import styled from 'styled-components';
 
 
 export const query = graphql`
@@ -31,13 +32,14 @@ export const query = graphql`
 `;
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <ul style={{
-			listStyle: "none",
-	}}>
-      {data.allSanityPost.edges.map(({ node: post }) => (
-        <li key={post.publishedAt}>
-					<h2 style={{
+	<Layout>
+		<BannerWapper>
+			<h1 id="banner">Jenelle's Photo A Day</h1>
+		</BannerWapper>
+		<ul style={{ listStyle: "none" }} >
+			{data.allSanityPost.edges.map(({ node: post }) => (
+				<li key={post.publishedAt}>
+					{/*		<h2 style={{
 						textAlign: "center",
 						
 						flex: '1 45%',
@@ -47,10 +49,86 @@ const IndexPage = ({ data }) => (
 					}}>{post.title}</h2>
           <Image fluid={post.mainImage.asset.fluid} alt={post.title} />
 					
-        </li>
-      ))}
-    </ul>
-  </Layout>
+
+				*/}
+					<DisplayWrapper>
+						<div id="container">
+
+
+							<div id="title">
+								{post.title}
+							</div>
+							<div id="image">
+								<Image fluid={post.mainImage.asset.fluid} alt={post.title} />
+							</div>
+							<div id="text">
+								{post.description}
+							</div>
+						</div>
+
+
+					</DisplayWrapper>
+
+				</li>
+			))}
+		</ul>
+
+
+
+	</Layout>
 );
 
 export default IndexPage;
+
+const BannerWapper = styled.div`
+#banner {
+	text-align: center;
+	font-size:1.5rem;
+	margin: 1rem;
+	letter-spacing: 0.1rem;
+}
+`
+
+const DisplayWrapper = styled.div`
+#container{
+	/* grid area declarations */
+
+	#title {grid-area: title; }
+	#image {grid-area:image;  }
+	#text {grid-area:text;  }
+
+	display: grid;
+	grid-template-columns: 1fr 5fr 1fr;
+	grid-template-rows: auto;
+	grid-gap: 10px;
+	background-color: black;
+	color: white;
+
+		grid-template-areas: 
+	
+	" . title     . "
+	" . image   . "
+	" . text  . ";
+
+	#title{
+margin:0.5rem 0 0;
+text-align: center;
+text-transform: capitalize;
+font-size: 0.8rem;
+letter-spacing:0.1rem;
+line-height: 0.9rem;
+	}
+
+	#image{
+
+	}
+#text{
+	margin: 0.5rem 0;
+	font-size: 0.8rem;
+	letter-spacing: 0.1rem;
+	line-height: 0.9rem;
+
+}
+
+}
+`
