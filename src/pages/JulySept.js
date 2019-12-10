c6a23b74ab170e9bd25ca80c3cdc1cffb2e14210
F John De Costa <john@mayerfleur.com>
2019-12-10 13:54:49 +1000

    julysept change sort order to ascending

--- src/pages/JulySept.js
+++ src/pages/JulySept.js
@@ -7,7 +7,7 @@ import styled from "styled-components";
 export const query = graphql`
 	{
 		allSanityPost(
-			sort: { fields: [publishedAt], order: [DESC] }
+			sort: { fields: [publishedAt], order: [ASC] }
 			skip: 92
 			limit: 92
 		) {
