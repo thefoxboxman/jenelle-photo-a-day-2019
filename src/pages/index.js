9263d3f726787daaaa3b1bbfadfbb8c43061b085
F John De Costa <john@mayerfleur.com>
2019-12-10 11:03:04 +1000

    Update subtitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,12 +35,13 @@ const IndexPage = ({ data }) => (
 			<h1 id="banner">Jenelle's Photo A Day</h1>
 		</BannerWapper>
 		<SubTitleWapper>
-			<h2>One photo taken each day through 2019</h2>
+			<h2 id="subtitle">One photo taken each day through 2019</h2>
 		</SubTitleWapper>
 		<ul style={{ listStyle: "none" }}>
 			{data.allSanityPost.edges.map(({ node: post }) => (
 				<li key={post._id}>
 					{/*		<h2 style={{
+
 						textAlign: "center",
 
 						flex: '1 45%',
