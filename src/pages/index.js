91a3748f9e539e04afa6ec258e701440e81e5688
F John De Costa <john@mayerfleur.com>
2019-12-10 10:04:40 +1000

    Added subtitle to main header

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,78 +2,68 @@ import React from "react";
 import Image from "gatsby-image";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
-import styled from 'styled-components';
-
+import styled from "styled-components";
 
 export const query = graphql`
-         {
-           allSanityPost(sort: { fields: [publishedAt], order: [DESC]}, limit: 50) {
-             edges {
-               node {
-                 _id
-                 title
-                 description
-                 publishedAt
-                 slug {
-                   current
-                 }
-                 mainImage {
-                   asset {
-                     fluid {
-                       ...GatsbySanityImageFluid
-                     }
-                   }
-                 }
-               }
-             }
-           }
-         }
-       `;
+	{
+		allSanityPost(sort: { fields: [publishedAt], order: [DESC] }, limit: 50) {
+			edges {
+				node {
+					_id
+					title
+					description
+					publishedAt
+					slug {
+						current
+					}
+					mainImage {
+						asset {
+							fluid {
+								...GatsbySanityImageFluid
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+`;
 
 const IndexPage = ({ data }) => (
 	<Layout>
 		<BannerWapper>
 			<h1 id="banner">Jenelle's Photo A Day</h1>
 		</BannerWapper>
-		<ul style={{ listStyle: "none" }} >
-		{data.allSanityPost.edges.map(({ node: post }) => (
+		<SubTitleWapper>
+			<h2>One photo taken each day through 2019</h2>
+		</SubTitleWapper>
+		<ul style={{ listStyle: "none" }}>
+			{data.allSanityPost.edges.map(({ node: post }) => (
 				<li key={post._id}>
 					{/*		<h2 style={{
 						textAlign: "center",
-						
+
 						flex: '1 45%',
 						flexWrap: 'wrap',
 						maxWidth: '45%',
 						margin: '2rem'
 					}}>{post.title}</h2>
           <Image fluid={post.mainImage.asset.fluid} alt={post.title} />
-					
+
 
 				*/}
 					<DisplayWrapper>
 						<div id="container">
-
-
-							<div id="title">
-								{post.title}
-							</div>
+							<div id="title">{post.title}</div>
 							<div id="image">
 								<Image fluid={post.mainImage.asset.fluid} alt={post.title} />
 							</div>
-							<div id="text">
-								{post.description}
-							</div>
+							<div id="text">{post.description}</div>
 						</div>
-
-
 					</DisplayWrapper>
-
 				</li>
 			))}
 		</ul>
-
-
-
 	</Layout>
 );
 
@@ -82,60 +72,72 @@ export default IndexPage;
 /* CSS in JS Style Components */
 
 const BannerWapper = styled.div`
-#banner {
-	text-align: center;
-	font-size:1.5rem;
-	margin: 1rem;
-	letter-spacing: 0.1rem;
-	font-family: Georgia, 'Times New Roman', Times, serif;
-}
-`  /* end BannerWrapper */
+	#banner {
+		text-align: center;
+		font-size: 1.5rem;
+		margin: 1rem;
+		letter-spacing: 0.1rem;
+		font-family: Georgia, "Times New Roman", Times, serif;
+	}
+`; /* end BannerWrapper */
+
+const SubTitleWapper = styled.div`
+	#subtitle {
+		text-align: center;
+		font-size: 1.1rem;
+		margin: 1rem;
+		letter-spacing: 0.1rem;
+		font-family: Georgia, "Times New Roman", Times, serif;
+	}
+`; /* end BannerWrapper */
 
 const DisplayWrapper = styled.div`
-#container{
-	/* grid area declarations */
+	#container {
+		/* grid area declarations */
 
-	#title {grid-area: title; }
-	#image {grid-area:image;  }
-	#text {grid-area:text;  }
+		#title {
+			grid-area: title;
+		}
+		#image {
+			grid-area: image;
+		}
+		#text {
+			grid-area: text;
+		}
 
-	display: grid;
-	grid-template-columns: 1fr 1fr 20fr 1fr 1fr;
-	grid-template-rows: auto;
-	grid-gap: 10px;
-	background-color: black;
-	color: white;
+		display: grid;
+		grid-template-columns: 1fr 1fr 20fr 1fr 1fr;
+		grid-template-rows: auto;
+		grid-gap: 10px;
+		background-color: black;
+		color: white;
 
-		grid-template-areas: 
-	
-	" . . title     . . "
-	" . image image  image . "
-	" . . text  . . ";
+		grid-template-areas:
+			" . . title     . . "
+			" . image image  image . "
+			" . . text  . . ";
 
-	#title{
-		color: hsla(0, 22%, 93%, 1.0);
-margin:0.5rem 0 0;
-padding-top: 2.0rem;
-text-align: center;
-text-transform: capitalize;
-font-size: 1.0rem;
-letter-spacing:0.1rem;
-line-height: 0.9rem;
-font-family: Georgia, 'Times New Roman', Times, serif;
+		#title {
+			color: hsla(0, 22%, 93%, 1);
+			margin: 0.5rem 0 0;
+			padding-top: 2rem;
+			text-align: center;
+			text-transform: capitalize;
+			font-size: 1rem;
+			letter-spacing: 0.1rem;
+			line-height: 0.9rem;
+			font-family: Georgia, "Times New Roman", Times, serif;
+		}
+
+		#image {
+		}
+		#text {
+			color: hsla(0, 22%, 93%, 0.8);
+			margin: 0.5rem 0;
+			font-size: 0.7rem;
+			letter-spacing: 0.1rem;
+			line-height: 0.9rem;
+			font-family: Georgia, "Times New Roman", Times, serif;
+		}
 	}
-
-	#image{
-
-	}
-#text{
-	color: hsla(0, 22%, 93%, 0.8);
-	margin: 0.5rem 0;
-	font-size: 0.7rem;
-	letter-spacing: 0.1rem;
-	line-height: 0.9rem;
-	font-family: Georgia, 'Times New Roman', Times, serif;
-
-}
-
-}
-` /* end DisplayWrapper */
+`; /* end DisplayWrapper */
