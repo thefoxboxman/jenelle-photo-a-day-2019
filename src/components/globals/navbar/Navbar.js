6c2894074472e6098ef9ee308c6d973623a26070
John <john@John-New-MacPro.local>
2020-03-13 18:46:26 +1000

    removed usestate reference because it was not being used

--- src/components/globals/navbar/Navbar.js
+++ src/components/globals/navbar/Navbar.js
@@ -1,88 +1,76 @@
-import React, { useState } from "react";
-import { Link } from "gatsby";
-import styled from "styled-components";
+import React from "react"
+import { Link } from "gatsby"
+import styled from "styled-components"
 
 const Navbar = () => {
-	//set initial state navbar to false ie closed
+  return (
+    <NavWrapper>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/JanMar" className="nav-link">
+              Jan-March
+            </Link>
+          </li>
+          <li>
+            <Link to="/AprJun" className="nav-link">
+              April-June
+            </Link>
+          </li>
+          <li>
+            <Link to="/JulySept" className="nav-link">
+              July-Sept
+            </Link>
+          </li>
+          <li>
+            <Link to="/OctDec" className="nav-link">
+              Oct-Dec
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </NavWrapper>
+  )
+}
 
-	const [navbarOpen, setNavbarOpen] = useState(false);
-
-	//method to handle navbar action
-	// handleNavbar = () => {
-	//  this.setState(() => {
-	//   return {
-	//   navbarOpen: !this.state.navbarOpen
-	// };
-	//});
-	// };
-	return (
-		<NavWrapper>
-			<nav className="navbar">
-				<ul>
-					<li>
-						<Link to="/" className="nav-link">
-							Home
-						</Link>
-					</li>
-					<li>
-						<Link to="/JanMar" className="nav-link">
-							Jan-March
-						</Link>
-					</li>
-					<li>
-						<Link to="/AprJun" className="nav-link">
-							April-June
-						</Link>
-					</li>
-					<li>
-						<Link to="/JulySept" className="nav-link">
-							July-Sept
-						</Link>
-					</li>
-					<li>
-						<Link to="/OctDec" className="nav-link">
-							Oct-Dec
-						</Link>
-					</li>
-				</ul>
-			</nav>
-		</NavWrapper>
-	);
-};
-
-export default Navbar;
+export default Navbar
 
 //style the navbar with media query
 const NavWrapper = styled.div`
-	.navbar {
-		padding-top: 15px;
-		border-top: solid 0.1rem white;
-		border-bottom: solid 0.1rem white;
-	}
-	ul {
-		display: flex;
-		list-style-type: none;
-		margin: 0 auto;
-		padding: 0;
-		justify-content: center;
-		align-items: center;
-	}
+  .navbar {
+    padding-top: 15px;
+    border-top: solid 0.1rem white;
+    border-bottom: solid 0.1rem white;
+  }
+  ul {
+    display: flex;
+    list-style-type: none;
+    margin: 0 auto;
+    padding: 0;
+    justify-content: center;
+    align-items: center;
+  }
 
-	.nav-link {
-		text-decoration: none;
-		color: white;
+  .nav-link {
+    text-decoration: none;
+    color: white;
 
-		border: solid 0.1rem white;
-		border-radius: 0.3rem;
-		font-size: 0.9rem;
-		padding: 0.1rem 0.5rem;
-		margin: 1rem;
-		text-transform: capitalize;
-	}
-	.nav-link:hover {
-		font-size: 1.1rem;
-		background: black;
-		color: white;
-		cursor: pointer;
-	}
-`;
+    border: solid 0.1rem white;
+    border-radius: 0.3rem;
+    font-size: 0.9rem;
+    padding: 0.1rem 0.5rem;
+    margin: 1rem;
+    text-transform: capitalize;
+  }
+  .nav-link:hover {
+    font-size: 1.1rem;
+    background: black;
+    color: white;
+    cursor: pointer;
+  }
+`
